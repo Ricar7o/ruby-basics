9e0d0f62322f09203cf420aedc7afe292619a9c1
Ricardo Cervantes <ricardo07@gmail.com>
2013-06-18 14:37:11 -0500

    Worked on the bank.rb file and added instance methods that control deposits, withdrawals and loans

--- Day07/bank.rb
+++ Day07/bank.rb
@@ -1,7 +1,13 @@
 class BankAccount
 	######## Class level ##########
-	@@interest_rate
-	@@outstanding_loans
+	@@interest_rate = 0.06
+	@@outstanding_loans = 0.0
+
+	def self.get_outstanding_loans
+		@@outstanding_loans
+	end
+
+
 
 	####### Instance level ##########
 	attr_accessor :name, :balance, :loan
@@ -10,6 +16,7 @@ class BankAccount
 		raise ArgumentError unless amount.to_f > 0 #This is throwing an error to the user if the amount can't be converted to a float
 		@name = name
 		@balance = amount.to_f
+		@loan = 0.0
 	end
 
 	def deposit_money(amount)
@@ -26,18 +33,47 @@ class BankAccount
 	end
 
 	def get_balance
+		if @loan > 0
+			puts "#{@name} has a loan for $#{@loan}"
+		end
 		puts "The balance in #{@name}'s account is:"
 		@balance.round(2)
 	end
 
 	def take_loan(amount)
-		raise ArgumentError, 'Please provide a positive number.' unless amount.to_f > 0
+		raise ArgumentError, "Please provide a positive number." unless amount.to_f > 0
 		@@outstanding_loans += amount.to_f
-		@loan += amount
+		@loan += amount.to_f
+	end
+
+	def repay_loan(amount)
+		raise ArgumentError, "Please provide a positive number" unless amount.to_f > 0
+		if amount > @loan
+			@@outstanding_loans -= @loan
+			puts "Your loan has been fully repaid. Here's your change: $#{amount.to_f - @loan}"
+			@loan = 0
+		else
+			@@outstanding_loans -= amount.to_f
+			@loan -= amount
+		end
+		puts "Loan balance:"
+		@loan
+	end
+
+	def accrue_interest
+		if @loan <= 0
+			puts "#{@name} has no money loaned"
+		end
+		#This code is executed either way because in case of 0, it doesn't affect balances
+		@@outstanding_loans += @loan * (1 + @@interest_rate) 
+		@loan *= (1 + @@interest_rate)
+		@loan.round(2)
+	end
+
 end
 
 # Code to initialize variables and improve testing time
-
+system "clear"
 ricardo = BankAccount.new("Ricardo")
 sonny = BankAccount.new("Sonny",200)
 ale = BankAccount.new("Ale",300)
